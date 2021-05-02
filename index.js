3898e0966e1951ac6b44d2b1c63cbe05d3b8a12a
Pyeroh <quentin.nanteuil@gmail.com>
2021-05-03 08:33:15 +1100

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
-// Import stylesheets
-import './style.css';
+const graphContainer = document.getElementById("graph-container");
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const options = {
+  template: GitgraphJS.templateExtend("metro", {
+    commit: {
+      message: {
+        displayAuthor: false,
+        displayHash: false
+      }
+    }
+  })
+};
+
+function createStyle(color) {
+  return {
+    style: {
+      color: color,
+      dot: {
+        color: color
+      },
+      message: {
+        color: "transparent"
+      },
+      label: {
+        color: color,
+        strokeColor: color
+      }
+    }
+  };
+}
+
+function emptyCommit(color) {
+  return {
+    subject: " _",
+    ...createStyle(color)
+  };
+}
+
+const gitgraph = GitgraphJS.createGitgraph(graphContainer, options);
+
+const master = gitgraph.branch("master");
+master.commit("").commit("");
+
+const feature1 = master.branch({ name: "feature/1", ...createStyle("red") });
+feature1.commit(emptyCommit("red"));
+
+const orangeStyle = createStyle("#f1c109");
+const feature2 = master.branch({
+  name: "feature/2",
+  ...createStyle("#f1c109")
+});
+feature2.commit(emptyCommit("#f1c109"));
+
+const purpleStyle = createStyle("purple");
+const feature3 = master.branch({ name: "feature/3", ...createStyle("purple") });
+feature3.commit(emptyCommit("purple"));
+
+master
+  .merge(feature1)
+  .merge(feature2)
+  .merge(feature3);
